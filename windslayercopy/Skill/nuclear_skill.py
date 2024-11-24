9abc3d207e14a67b78db9838b4aa0efdab054eb2
YJ0717 <cafricool3@naver.com>
2024-11-25 03:36:11 +0900

    궁극기 추가

--- windslayercopy/Skill/nuclear_skill.py
+++ windslayercopy/Skill/nuclear_skill.py
@@ -4,27 +4,23 @@ import gfw.image as image
 import config
 import time
 
-#=================== 스킬시전 클래스와 투사체 클래스를 분류하여 기존에 있던 버그 수정 해결 ===================
-
-#============== 땅스킬시전 업데이트 및 그리기 기능 ==================================================
-class StoneSkill:
-    def __init__(self, cast_image_file='stone_attack.png', skill_image_file='stone_skill.png'):
+class NuclearSkill:
+    def __init__(self, cast_image_file='nuclear_attack.png', skill_image_file='nuclear_skill.png'):
         self.cast_image = image.load(cast_image_file)
         self.skill_image = image.load(skill_image_file)
         self.is_casting = False
         self.cast_time = 0
-        self.cast_duration = config.STONE_CAST_DURATION
+        self.cast_duration = config.NUCLEAR_CAST_DURATION
         self.frame = 0
-        self.cast_frame_count = config.STONE_CAST_FRAME_COUNT
-        self.cast_frame_width = config.STONE_CAST_FRAME_WIDTH
-        self.cast_frame_height = config.STONE_CAST_FRAME_HEIGHT
-        self.cast_fps = config.STONE_CAST_FPS
-        self.projectiles = []  
-        self.skill_cooldown = config.STONE_SKILL_COOLDOWN
+        self.cast_frame_count = config.NUCLEAR_CAST_FRAME_COUNT
+        self.cast_frame_width = config.NUCLEAR_CAST_FRAME_WIDTH
+        self.cast_frame_height = config.NUCLEAR_CAST_FRAME_HEIGHT
+        self.cast_fps = config.NUCLEAR_CAST_FPS
+        self.projectiles = []
+        self.skill_cooldown = config.NUCLEAR_SKILL_COOLDOWN
         self.last_skill_time = -self.skill_cooldown
-        self.spawn_offset_x = config.STONE_SKILL_SPAWN_OFFSET_X  # 스킬 소환 x축 오프셋
+        self.spawn_offset_x = config.NUCLEAR_SKILL_SPAWN_OFFSET_X
 
-    #==================== 스킬 시전 중인지 아닌지 확인하고 투사체클래스에게 정보 전달================   
     def start_cast(self, x, y, direction):
         current_time = time.time()
         if self.is_casting or current_time - self.last_skill_time < self.skill_cooldown:
@@ -40,7 +36,6 @@ class StoneSkill:
 
         return True
 
-    # ==================== 스킬 시전 애니메이션이 나오면 투사체 나오도록 업데이트 설정 =======================
     def update(self):
         if self.is_casting:
             self.cast_time += gfw.frame_time
@@ -50,46 +45,41 @@ class StoneSkill:
             else:
                 self.frame = int(self.cast_time * self.cast_fps) % self.cast_frame_count
 
-        # 투사체이기때문에 x범위로 사라지는것이 아닌 지속시간으로 삭제
         self.projectiles = [p for p in self.projectiles if p.update()]
 
-    # ==================== 스킬 시전 애니메이션 그리기 기능 ==================================================
     def draw(self, x, y, flip='h'):
         if self.is_casting:
             x_offset = self.frame * self.cast_frame_width
             self.cast_image.clip_composite_draw(
                 x_offset, 0, self.cast_frame_width, self.cast_frame_height,
-                0, flip, x, y+30,
+                0, flip, x, y,
                 self.cast_frame_width, self.cast_frame_height
             )
         for projectile in self.projectiles:
             projectile.draw()
 
-# ==================== 투사체 클래스 설정 ==================================================
 class Projectile:
     def __init__(self, x, y, direction, image):
         self.x = x
-        self.y = y +10
+        self.y = y + 120
         self.direction = direction
         self.image = image
         self.frame = 0
-        self.frame_count = config.STONE_SKILL_FRAME_COUNT
-        self.frame_width = config.STONE_SKILL_FRAME_WIDTH
-        self.frame_height = config.STONE_SKILL_FRAME_HEIGHT
-        self.fps = config.STONE_SKILL_FPS
+        self.frame_count = config.NUCLEAR_SKILL_FRAME_COUNT
+        self.frame_width = config.NUCLEAR_SKILL_FRAME_WIDTH
+        self.frame_height = config.NUCLEAR_SKILL_FRAME_HEIGHT
+        self.fps = config.NUCLEAR_SKILL_FPS
         self.elapsed_time = 0
-        self.duration = config.STONE_SKILL_DURATION  # 지속시간 설정
+        self.duration = config.NUCLEAR_SKILL_DURATION
 
-    # ==================== 투사체 업데이트 기능 ==================================================
     def update(self):
         self.elapsed_time += gfw.frame_time
         self.frame = int(self.elapsed_time * self.fps) % self.frame_count
 
         if self.elapsed_time > self.duration:
-            return False  
+            return False
         return True
 
-    # ==================== 투사체 그리기 기능 ==================================================
     def draw(self):
         x_offset = self.frame * self.frame_width
         flip = 'h' if self.direction > 0 else ''
