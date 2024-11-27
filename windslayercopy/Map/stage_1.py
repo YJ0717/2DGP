b841ab13f34527a6f1c5e38405279ac4471936ae
YJ0717 <cafricool3@naver.com>
2024-11-28 05:44:58 +0900

    2번째 npc추가 npc에 가서 방향키 위쪽을 눌러 말풍선을 확인해야만 무기를 장착할 수 있게 만들고 무기를 장착할때만 포탈에 들어가지도록 설정

--- windslayercopy/Map/stage_1.py
+++ windslayercopy/Map/stage_1.py
@@ -36,12 +36,29 @@ class TileMap:
 
         self.talk_image = image.load('npc_talk1.png')
 
+        #===============================두 번째 NPC 도입==========================================
+        self.npc2_image = image.load('npc_02.png')  # 두 번째 NPC 이미지 로드
+        self.npc2_x = 2000  # 두 번째 NPC의 x 좌표
+        self.npc2_y = 250  # 두 번째 NPC의 y 좌표
+        self.npc2_frame = 0
+        self.npc2_frame_count = 3
+        self.npc2_frame_width = 87
+        self.npc2_frame_height = 160
+        self.npc2_fps = 2
+        self.npc2_time = 0
+
+        self.npc2_collision_x = 1000
+        self.npc2_collision_y = 232
+
+        self.talk2_image = image.load('npc_talk2.png')  # 두 번째 NPC의 말풍선 이미지 로드
+
 # ====================충돌처리 및 애니메이션을 위한 업데이트 ================
     def update(self):
         self.x_offset = -self.player.x  
         self.check_collision()
         self.update_portal_animation()  
         self.update_npc_animation()
+        self.update_npc2_animation()
 
     def update_portal_animation(self):
         self.portal_animation_frame += gfw.frame_time * self.portal_animation_speed
@@ -51,6 +68,10 @@ class TileMap:
     def update_npc_animation(self):
         self.npc_time += gfw.frame_time
         self.npc_frame = int(self.npc_time * self.npc_fps) % self.npc_frame_count
+
+    def update_npc2_animation(self):
+        self.npc2_time += gfw.frame_time
+        self.npc2_frame = int(self.npc2_time * self.npc2_fps) % self.npc2_frame_count
 
 #======================== 충돌처리 ======================
     def check_collision(self):
@@ -96,7 +117,11 @@ class TileMap:
             player_x + player_half_width > portal_x - half_width and
             player_y - player_half_height < portal_y + half_height and
             player_y + player_half_height > portal_y - half_height):
-            self.player.near_portal = True
+
+            if self.player.weapon_equipped:  # 무기를 장착했을때만 포탈이동 
+                self.player.near_portal = True
+            else:
+                self.player.near_portal = False
         else:
             self.player.near_portal = False
 
@@ -110,6 +135,17 @@ class TileMap:
             self.player.near_npc = True
         else:
             self.player.near_npc = False
+
+        # ================== 두 번째 NPC와의 충돌 체크 =======================
+        npc2_half_width = self.npc2_frame_width // 2
+        npc2_half_height = self.npc2_frame_height // 2
+        if (player_x - player_half_width < self.npc2_collision_x + npc2_half_width and
+            player_x + player_half_width > self.npc2_collision_x - npc2_half_width and
+            player_y - player_half_height < self.npc2_collision_y + npc2_half_height and
+            player_y + player_half_height > self.npc2_collision_y - npc2_half_height):
+            self.player.near_npc2 = True
+        else:
+            self.player.near_npc2 = False
 
 #======================== 타일 그리기 ======================
     def draw(self):
@@ -145,6 +181,14 @@ class TileMap:
         # 말풍선 
         if self.player.near_npc:
             self.talk_image.draw(self.npc_x + self.x_offset, self.npc_y + self.npc_frame_height + 20)
+
+        npc2_x_offset = self.npc2_frame * self.npc2_frame_width
+        self.npc2_image.clip_draw(npc2_x_offset, 0, self.npc2_frame_width, self.npc2_frame_height,
+                                  self.npc2_x + self.x_offset, self.npc2_y)
+
+        # 두 번째 NPC의 말풍선
+        if self.player.talk_to_npc2:
+            self.talk2_image.draw(self.npc2_x + self.x_offset, self.npc2_y + self.npc2_frame_height + 20)
 
 def get_tile_map():
     # 타일 이미지 로드
