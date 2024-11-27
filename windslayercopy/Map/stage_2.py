419ea7a1ae130137d966f30ae4c20ffae819ba6b
YJ0717 <cafricool3@naver.com>
2024-11-27 12:48:21 +0900

    스테이지2에 백그라운드 추가 및 weapon을 항상 장착하도도록 설정

--- windslayercopy/Map/stage_2.py
+++ windslayercopy/Map/stage_2.py
@@ -4,11 +4,28 @@ from player import CustomPlayer
 
 world = gfw.World(['background', 'tile', 'player'])
 
+class Background:
+    def __init__(self):
+        self.image = gfw.image.load('background.png')
+        self.x = 0
+        self.y = 0
+
+    def update(self):
+        global player
+        self.x = -player.x * 0.5
+        self.y = -player.y * 0.1
+
+    def draw(self):
+        self.image.draw_to_origin(self.x, self.y, 3000, 1250)
+
 def enter():
-    global player, tile_map
+    global player, background, tile_map
     player = CustomPlayer()
+    player.equip_weapon()
+    background = Background()
     tile_images, tile_map_data = get_tile_map()
     tile_map = TileMap(tile_images, tile_map_data, 32, player)
+    world.append(background, world.layer.background)
     world.append(tile_map, world.layer.tile)
     world.append(player, world.layer.player)
 
